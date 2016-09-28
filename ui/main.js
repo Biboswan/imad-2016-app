1ae4cea40e2a962a74d50beb92e5b9e8a3c62126
Biboswan <biboswan98@gmail.com>
2016-09-28 20:14:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,3 +19,17 @@ button.onclick=function()
     request.open('GET','http://biboswan.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+var Inputnam=document.getElementById('name');
+var name=Inputnam.value;
+document.getElementById('accept').onclick=function()
+{
+    var namarr=[name1,name2,name3,name4];
+    var list='';
+    for(var i=0;i<namarr.length;i++)
+    {
+        list+='<li>'+namar[i]+'</li>';
+    }
+    var ol=document.getElementById('list');
+    ol.innerHTML=list;
+    
+}
