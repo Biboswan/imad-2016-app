5e350dc6828d25cf3043fc55c2504aad42cb82bf
Biboswan <biboswan98@gmail.com>
2016-09-28 16:55:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
-console.log("Loaded!");
+console.log('Loaded!');
+var count=0;
+var button=document.getElementById('press');
+button.oneclick=function()
+{
+    count=count+1;
+    document.getElementById('count').innerHTML=count.toString();
+}
