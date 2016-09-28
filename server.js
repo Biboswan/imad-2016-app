5353cabac0ef3856d326490d4fbf5db442c3d45a
Biboswan <biboswan98@gmail.com>
2016-09-28 16:34:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var counter =0;
 
 var app = express();
 app.use(morgan('combined'));
@@ -19,6 +20,8 @@ app.get('/ui/style.css', function (req, res) {
 });
 app.get('/ui/me1', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'me1.png'));
+  app.get('/counter', function (req, res) {
+  res.send(counter++.toString);
 });
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
