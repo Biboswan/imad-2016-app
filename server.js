8c97a52f619efa876219eb82bf16ba9259b214e8
Biboswan <biboswan98@gmail.com>
2016-10-16 13:57:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -36,6 +36,8 @@ app.get('/submit-comment', function (req, res) {//submit-name?name=xxxx
 });
 app.get('/ui/open', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'open.html'));})
+  app.get('/p1.jpg', function (req, res) {
+     res.sendFile(path.join('p1.jpg'));})
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
