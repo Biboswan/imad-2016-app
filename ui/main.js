bfde1be19b6f006c3f8ac220dd9b9f681ce65537
Biboswan <biboswan98@gmail.com>
2016-10-16 20:24:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,21 @@
 
+var path=window.location.pathname;
+if(path==='/ui/home')
+{
+    var button=document.getElementById('press');
+    button.onclick=function()
+{
+    isadder('count');
+    
+}
+}
 
+isadder('c'); 
+
+
+
+var isadder=function(id)
+{
  var request= new XMLHttpRequest();
      
     request.onreadystatechange=function()
@@ -8,32 +24,16 @@
         {
         if(request.status===200)
         {
-        var x=request.responseText;
-        document.getElementById('c').innerHTML=x.toString();
+        var counter=request.responseText;
+        document.getElementById(id).innerHTML=counter.toString();
         }
         }
     }
-    request.open('GET','http://biboswan.imad.hasura-app.io/visitor',true);
+    request.open('GET','http://biboswan.imad.hasura-app.io${path}',true);
      request.send(null);
+}
+
        
 
-var button=document.getElementById('press');
-button.onclick=function()
-{
-    var request= new XMLHttpRequest();
-     request.open('GET','http://biboswan.imad.hasura-app.io/counter',true);
-     request.send(null);
-    request.onreadystatechange=function()
-    {
-        if(request.readyState===XMLHttpRequest.DONE)
-        {
-        if(request.status===200)
-        {
-        var counter=request.responseText;
-        var span=document.getElementById('count');
-        span.innerHTML=counter.toString();
-    }
-        }
-    }
 
-   }
+
