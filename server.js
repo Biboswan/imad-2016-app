bf8ebc7e05fafaa0649af494aea18841672fbbd5
Biboswan <biboswan98@gmail.com>
2016-10-22 20:55:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'open.html'));
 });
-app.get('/ui/A1', function (req, res) {
+app.post('/ui/A1', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'A1.html'));
 });
 app.get('/ui/A2', function (req, res) {
