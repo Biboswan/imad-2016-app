7e71a956288c235b45d48eaa91eaabfbce11902f
Biboswan <biboswan98@gmail.com>
2016-10-26 19:08:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var config =
 
 var pool = new Pool(config);
 app.get('/visited', function (req, res) {
-    pool.query('UPDATE "Visited" SET count=count+1 RETURNING count',function(err,result){    
+    pool.query('UPDATE "Visited" SET count=count+1',function(err,result){    
           res.send((result.rows[0].count).toString());
         
     });
