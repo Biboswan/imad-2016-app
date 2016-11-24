3daf99a926efe227cb555424debcd6efe2af433e
Biboswan <biboswan98@gmail.com>
2016-11-24 14:06:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -67,6 +67,37 @@ batt.innerHTML=battery.level*100+'%';
 }
 var interval=setInterval(battstatus,1000);
 
+var submit=document.getElementById('register').onclick=function(){
+            var request = new XMLHttpRequest();
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+          
+              if (request.status === 200) {
+                  alert('Account created successfully');
+                  submit.value = 'Submited!';
+              } else {
+                  alert('Could not create the account');
+              }
+          }
+        };
+                  
+                  var username = document.getElementById('username').value;
+                  var password = document.getElementById('pwd').value;
+                  var name = document.getElementById('name');
+                  var dob = document.getElementById('dob');
+                  var email = document.getElementById('email');
+                  var sex = document.getElementById('sex');
+                  request.open('POST', '/create-user', true);
+                  request.setRequestHeader('Content-Type', 'application/json');
+                  request.send(JSON.stringify({username: username, password: password,name:name,dob:dob,email:email,sex:sex}));  
+                  submit.value = 'Submiting...';
+    
+    };
+
+                  
+            
+
+
 
 
       
