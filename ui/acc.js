bd9407ca1975192231ff3ff223ee0f3c787877d4
Biboswan <biboswan98@gmail.com>
2016-11-24 20:09:45 +0530

    [imad-console] Updates ui/acc.js

--- ui/acc.js
+++ ui/acc.js
@@ -22,10 +22,15 @@
                   var name = document.getElementById('name').value;
                   var dob = document.getElementById('dob').value;
                   var email = document.getElementById('email').value;
-                  var sex = document.getElementById('sex').value;
+                  var i,sex;
+                  for(i=1;i<=3;i++)
+                  {
+                  sex = document.getElementById('sex'+i);
+                  if(sex.checked===true){break;}
+                  }
                   request.open('POST', '/create-user', true);
                   request.setRequestHeader('Content-Type', 'application/json');
-                  request.send(JSON.stringify({username: username, password: password,name:name,dob:dob,email:email,sex:sex}));  
+                  request.send(JSON.stringify({username: username, password: password,name:name,dob:dob,email:email,sex:sex.value}));  
                   submit.value = 'Submiting...';
                    
     
