b80edc058658ebb0e3e9cfd211c58eb9172c85e3
Biboswan <biboswan98@gmail.com>
2016-11-25 00:30:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -66,6 +66,37 @@ batt.innerHTML=battery.level*100+'%';
 })
 }
 var interval=setInterval(battstatus,1000);
+var submit=document.getElementById('login-btn');
+          submit.onclick = function(){
+            var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  submit.value = 'Logged!';
+              } else if (request.status === 403) {
+                  submit.value = 'Invalid credentials. Try again?';
+              } else if (request.status === 500) {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              } else {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              }
+             // loadLogin();        
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        request.open('POST', '/login', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        submit.value = 'Logging in...';
+        
+    }
+
+          };
+};
 
          
       
