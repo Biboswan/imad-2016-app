c71f75fe246a3c7b918b0e6ca1c2426ce40d8679
Biboswan <biboswan98@gmail.com>
2016-11-25 01:22:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,4 @@
-var counter,content,request;
-       request= new XMLHttpRequest();
-    request.onreadystatechange=function()
-    {
-        if(request.readyState===XMLHttpRequest.DONE)
-        {
-        if(request.status===200)
-        {
-           counter=request.responseText;
-           content=document.getElementById('c')
-        content.innerHTML=counter.toString();
-        }
-        }
-    }
-     request.open('GET',`http://biboswan.imad.hasura-app.io/visited`,true);
-      request.send(null);
+
        navigator.getBattery().then(function(battery) {
          function updateAllBatteryInfo(){
            updateChargeInfo();
