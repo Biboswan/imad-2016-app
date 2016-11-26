2f9da7d4899a889375ec989d78069353d6a02f07
Biboswan <biboswan98@gmail.com>
2016-11-26 17:40:44 +0530

    Create open.js

---
+++ ui/open.js
@@ -0,0 +1,17 @@
+var counter,content,request;
+       request= new XMLHttpRequest();
+    request.onreadystatechange=function()
+    {
+        if(request.readyState===XMLHttpRequest.DONE)
+        {
+        if(request.status===200)
+        {
+           counter=request.responseText;
+           content=document.getElementById('c')
+        content.innerHTML=counter.toString();
+        }
+        }
+    }
+     request.open('GET',`http://biboswan.imad.hasura-app.io/visited`,true);
+     request.send(null);
+      
