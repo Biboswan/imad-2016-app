85291ceed1bc1966267e9391dfb23d9372e056db
Biboswan <biboswan98@gmail.com>
2016-12-02 18:11:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -181,12 +181,12 @@ app.get('/categorised',function(req,res){
     });
 });
 app.get('/:art_title',function(req,res){ 
- pool.query('SELECT content FROM "Articles" WHERE title=$1',[req.params.art_title],
+ pool.query('SELECT "Articles".content  FROM "Articles" WHERE title=$1',[req.params.art_title],
  function(err,result) {
         if (err) {
               res.status(500).send(err.toString());
            } else {
-               res.send(result.rows[0].content);
+               res.send((result.rows[0].content).toString());
            }
     });
 });
