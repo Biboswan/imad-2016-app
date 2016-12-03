43ee9c56cfac1ed042ac538aba2c0cfe66fd5aa5
Biboswan <biboswan98@gmail.com>
2016-12-03 16:28:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -245,7 +245,7 @@ app.get('/ui/home', function (req, res) {
  app.get('/ui/articles',function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'Articles.html'));});
   app.get('/bpln_pic',function (req, res) {
-  res.sendFile(path.join(__dirname, '', 'bpln_pic.JPG'));});
+  res.sendFile(path.join(__dirname, '', 'bpln_pic.png'));});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
